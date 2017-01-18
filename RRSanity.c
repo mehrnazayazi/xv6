f446a0e168497cf070b6837142c2d45fe47dd27b
mehrnaz.ayazi <mehrnaz.ayazi@gmail.com>
2017-01-18 11:17:03 +0330

    waitx

--- RRSanity.c
+++ RRSanity.c
@@ -6,14 +6,15 @@ void print() {
     int k;
     //int sizeOfArray = 100;
     printf(1, "\n");
-    for (k=0;k<1000;k++){
+    for (k=0;k<100;k++){
         printf(2, "Child %d baraye bare %d chap mishavad.\n",getpid(),k);
     }
 }
 
-void test(void) {
-    //int numberOfForks = 10;
-    int wTime[10];
+
+int main(void) {
+    //test();
+     int wTime[10];
     int rTime[10];
     int Id;
 
@@ -25,13 +26,10 @@ void test(void) {
             exit();
         }
     }
-    while(wait() > 0){
-            if(i<10)
+   // while(wait() > 0){
+   for(int i=0;i<10;i++)
+            //if(i<10)
         printf(2, "PID : %d -run time : %d - turn around time : %d - waiting time : %d.\n",getyear(&wTime[i],&rTime[i]),rTime[i],rTime[i]+wTime[i],wTime[i]);
-    }
-}
-
-int main(void) {
-    test();
+   // }
     exit();
 }
